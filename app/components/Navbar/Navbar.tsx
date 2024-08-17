42408a11ab6926fae217e805fc3ee98178f670f0
Seven <ahmadaliev.sd@gmail.com>
2024-08-17 14:14:54 +0900

    navbar items added

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import SearchFilter from "./SearchFilter";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+      <div className="max-w-[1500px] mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            {" "}
+            <Image src="/logo.png" alt="Logo" width={170} height={35} />
+          </Link>
+          <div className="flex space-x-6">
+            <SearchFilter/>
+          </div>
+
+          <div className="flex items-center space-x-6">add property</div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ app/components/Navbar/SearchFilter.tsx
@@ -0,0 +1,39 @@
+import {IoSearch} from 'react-icons/io5'
+
+type Props = {}
+
+const SearchFilter = (props: Props) => {
+  return (
+    <div className='h-[64px] flex flex-row items-center justify-between border rounded-full'>
+        <div className='hidden lg:block'>
+            <div className='flex flex-row items-center justify-between'>
+                <div className='cursor-pointer w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                    <p className='text-xs font-semibold'>Where</p>
+                    <p className='text-sm' > wanted location</p>
+                </div>
+                <div className='cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                    <p className='text-xs font-semibold'>Check in</p>
+                    <p className='text-sm' > add dates</p>
+                </div>
+                <div className='cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                    <p className='text-xs font-semibold'>Check out</p>
+                    <p className='text-sm' > add dates</p>
+                </div>
+                <div className='cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                    <p className='text-xs font-semibold'>Who</p>
+                    <p className='text-sm' > add guests</p>
+                </div>
+            </div>
+        </div>
+
+        <div className='p-2' >
+            <div className='p-4 bg-airbnb rounded-full text-white hover:bg-airbnb-dark cursor-pointer' >
+                <IoSearch/>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default SearchFilter
