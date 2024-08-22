33ef18af352b75d6248fe118a9745944d93af685
Seven <ahmadaliev.sd@gmail.com>
2024-08-22 14:20:24 +0900

    navbar ui completed

--- app/components/Navbar/SearchFilter.tsx
+++ app/components/Navbar/SearchFilter.tsx
@@ -4,22 +4,22 @@ type Props = {}
 
 const SearchFilter = (props: Props) => {
   return (
-    <div className='h-[64px] flex flex-row items-center justify-between border rounded-full'>
+    <div className='h-[48px] lg:h-[64px] flex flex-row items-center justify-between border rounded-full'>
         <div className='hidden lg:block'>
             <div className='flex flex-row items-center justify-between'>
-                <div className='cursor-pointer w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                <div className='cursor-pointer w-[250px] h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
                     <p className='text-xs font-semibold'>Where</p>
                     <p className='text-sm' > wanted location</p>
                 </div>
-                <div className='cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                <div className='cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
                     <p className='text-xs font-semibold'>Check in</p>
                     <p className='text-sm' > add dates</p>
                 </div>
-                <div className='cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                <div className='cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
                     <p className='text-xs font-semibold'>Check out</p>
                     <p className='text-sm' > add dates</p>
                 </div>
-                <div className='cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
+                <div className='cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100' >
                     <p className='text-xs font-semibold'>Who</p>
                     <p className='text-sm' > add guests</p>
                 </div>
--- app/components/Navbar/UserNav.tsx
+++ app/components/Navbar/UserNav.tsx
@@ -6,8 +6,8 @@ const UserNav = () => {
   return (
     <div className="p-2 relative inline-block border rounded-full">
         <button className="flex items-center">
-        <IoMdMenu />
-        <CiUser />
+        <IoMdMenu className="h-9 w-9" />
+        <CiUser className="h-8 w-8" />
 
         </button>
     </div>
