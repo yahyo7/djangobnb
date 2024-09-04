fbfa91cf381b213b985dee1ede34e1463d7e8a98
Seven <ahmadaliev.sd@gmail.com>
2024-09-04 13:11:03 +0900

    properties connected to django api

--- app/components/Properties/Properties.tsx
+++ app/components/Properties/Properties.tsx
@@ -1,15 +1,45 @@
+"use client";
 import React from "react";
 import PropertyItem from "./PropertyItem";
 
-type Props = {className?: string};
+import { useEffect, useState } from "react";
+
+type Props = { className?: string };
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_night: number;
+  image_url: string;
+}
 
 const Properties: React.FC<Props> = ({ className }) => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+
+  const getProperties = async () => {
+    const url = "http://localhost:8000/api/properties/";
+
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setProperties(json.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
   return (
     <div className={className}>
-      <PropertyItem />
-      <PropertyItem />
-      <PropertyItem />
-      <PropertyItem />
+      {properties.map((property) => (
+        <PropertyItem key={property.id} property={property} />
+      ))}
     </div>
   );
 };
--- app/components/Properties/PropertyItem.tsx
+++ app/components/Properties/PropertyItem.tsx
@@ -1,26 +1,31 @@
 import Image from "next/image";
 import React from "react";
+import { PropertyType } from "./Properties";
 
-type Props = {};
+interface PropertyProps {
+  property: PropertyType;
+}
 
-const PropertyItem = (props: Props) => {
+const PropertyItem: React.FC<PropertyProps> = ({
+  property
+}) => {
   return (
     <div className="cursor-pointer">
       <div className="relative overflow-hidden aspect-square rounded-xl">
         <Image
           alt="beach"
           fill
-          src="/images/beach-nature.jpg"
+          src={property.image_url}
           sizes="(max-width: 768px): 768px, (max-width: 1200px): 768px, 768px"
           className="hover:scale-110 object-cover transition h-full w-full"
         />
       </div>
 
       <div className="mt-2">
-        <p className="text-lg font-bold">Property name</p>
+        <p className="text-lg font-bold">{property.title}</p>
       </div>
       <div className="mt-2">
-        <p className="text-sm text-gray-500">Price 200$</p>
+        <p className="text-sm text-gray-500">${property.price_per_night}</p>
       </div>
     </div>
   );
